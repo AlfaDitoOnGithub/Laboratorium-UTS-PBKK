38c0e708cc1df94556e1ff01f53b7003792672be
AlfaDitoOnGithub <alfaditoauliadenova@gmail.com>
2023-10-24 14:51:11 +0700

    added interface phase1

---
+++ resources/views/inventory/create.blade.php
@@ -0,0 +1,25 @@
+@extends('inventory.layout')
+@section('content')
+
+<div class="card">
+    <div class="card-header">Inventory Page</div>
+    <div class="card-body">
+        <form action="{{ url('inventory')}}" method="post">
+            {!! csrf_field() !!}
+             <!-- $table->id();
+             $table->timestamps();
+             $table->string("jenisBarang");
+             $table->string("kondisiBarang");
+             $table->string("keteranganBarang");
+             $table->integer("jumlahBarang");
+             $table->string("fotoBarang");  -->
+
+            <label>Jenis Barang</label> <br>
+            <label>Kondisi Barang</label> <br>
+            <label>Keterangan</label> <br>
+            <label>Jumlah Barang</label> <br>
+            <label>Foto</label> <br>
+           
+        </form>
+    </div>
+</div>
